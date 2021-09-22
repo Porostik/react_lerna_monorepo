c2b3f0198df76e8915e619df1a81ac9eed23bc7f
Porostik <67925568+Porostik@users.noreply.github.com>
2021-09-22 18:04:30 +0300

    svg import test

--- packages/shared/src/components/Button/index.jsx
+++ packages/shared/src/components/Button/index.jsx
@@ -1,10 +1,12 @@
 import React, { useMemo } from 'react';
 
+import './button.scss';
+
 export const Button = ({ children, onClick }) => {
   const a = useMemo(() => {}, []);
 
   return (
-    <button type="button" onClick={onClick}>
+    <button className="button" type="button" onClick={onClick}>
       {children}
       {a}
     </button>
