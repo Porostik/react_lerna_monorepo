f9af8e83e67d251230025bf0dcc8c0fa96ea303d
Porostik <67925568+Porostik@users.noreply.github.com>
2021-09-26 23:42:27 +0300

    postcss fixes

--- packages/configs/postcss.config.js
+++ packages/configs/postcss.config.js
@@ -1,8 +1,13 @@
 /* eslint-disable*/
-
 module.exports = {
   plugins: [
-    require('autoprefixer'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      browsers: 'last 2 versions',
+    }),
     require('cssnano')({
       preset: 'default',
     }),
